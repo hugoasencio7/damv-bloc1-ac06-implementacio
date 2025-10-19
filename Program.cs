420ef62995fb88c65400b3f7188a1e989bd9858f
hugoasencio <hugo.asencio.7e9@itb.cat>
2025-10-19 14:00:16 +0200

    Subir EuroToDollar

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        /*The user is prompted to input an amount in euros.*/
+        const double exchange_rate = 1.12;
+        const string MSG_INPUT = "Enter amount in euros:";
+        const string MSG_RESULT = "Amount in euros:";
+
+        double euros, dollars;
+
+        Console.WriteLine(MSG_INPUT);
+        euros = Convert.ToDouble(Console.ReadLine());
+        dollars = euros * exchange_rate;
+        Console.WriteLine(MSG_RESULT + " " + dollars);
+        /*'dollars' contains the converted amount, and it is displayed.*/
+    }
+}
