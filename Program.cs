19c45c678efbf0cc3b979cda884a475910d5fa3c
hugoasencio <hugo.asencio.7e9@itb.cat>
2025-10-19 13:44:30 +0200

    Subir RectangleArea

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        /*The user is prompted to input the width and height of a rectangle.*/
+        const string MSG_INPUT_WIDTH = "Enter the width of the rectangle:";
+        const string MSG_INPUT_HEIGHT = "Enter the height of the rectangle:";
+        const string MSG_RESULT = "The area of the rectangle is:";
+
+        double width, height, area;
+
+        Console.WriteLine(MSG_INPUT_WIDTH);
+        width = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine(MSG_INPUT_HEIGHT);
+        height = Convert.ToDouble(Console.ReadLine());
+        area = width * height;
+        Console.WriteLine(MSG_RESULT + " " + area);
+        /*'area' contains the product of 'width' and 'height' and it is displayed*/
+    }
+}
